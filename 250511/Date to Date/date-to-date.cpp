7ec934dc781560319cd44ad8fe56313d9640f312
Sanghyun Ji <163238696+hmusk7@users.noreply.github.com>
2025-05-11 20:16:59 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250511/Date to Date/date-to-date.cpp
+++ 250511/Date to Date/date-to-date.cpp
@@ -6,7 +6,7 @@ int m1, d1, m2, d2;
 int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int sum_days(int month) {
-    int sum;
+    int sum = 0;
     for (int i = month - 1; i > 0; --i) {
         sum += num_of_days[i];
     }
