a8584941dc1f66d36c25a9065f3d43e08838cd4a
Sanghyun Ji <163238696+hmusk7@users.noreply.github.com>
2025-05-11 20:28:01 +0900

    Title: Date to Date, Time: 4ms, Memory: 0MB, Status: Passed - Codetree

--- 250511/Date to Date/date-to-date.cpp
+++ 250511/Date to Date/date-to-date.cpp
@@ -4,20 +4,17 @@ using namespace std;
 
 int m1, d1, m2, d2;
 int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-int sum_days(int month) {
-    int sum = 0;
-    for (int i = month - 1; i > 0; --i) {
-        sum += num_of_days[i];
-    }
-    return sum;
-}
+int prefix_days[13] = {};
 
 int main() {
     cin >> m1 >> d1 >> m2 >> d2;
 
     // Please write your code here.
-    cout << sum_days(m2) + d2 - sum_days(m1) - d1 + 1;
+    for (int i = 1; i < 13; ++i) {
+        prefix_days[i] = prefix_days[i - 1] + num_of_days[i];
+    }
+
+    cout << prefix_days[m2 - 1] + d2 - prefix_days[m1 - 1] - d1 + 1;
 
 
     return 0;
