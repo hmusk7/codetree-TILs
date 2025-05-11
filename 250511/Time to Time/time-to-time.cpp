489cd8073e6c5e46cb5bbc7361ddacd5851e374a
Sanghyun Ji <163238696+hmusk7@users.noreply.github.com>
2025-05-11 20:08:53 +0900

    Title: Time to Time, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250511/Time to Time/time-to-time.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c, d;
+
+int main() {
+    cin >> a >> b >> c >> d;
+
+    // Please write your code here.
+    cout << (a * 60 + b) - (c * 60 + d);
+
+    return 0;
+}
