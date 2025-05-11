5f57d4513dc7919bb2256b216b3287ba10fe85c4
Sanghyun Ji <163238696+hmusk7@users.noreply.github.com>
2025-05-11 20:39:44 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250511/DateTime to DateTime/datetime-to-datetime.cpp
+++ 250511/DateTime to DateTime/datetime-to-datetime.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int a, b, c;
 
 int date_to_min(int day, int hour, int min) {
-    if (day < 11 && hour < 11 && min < 11) return -1;
+    if (day < 11 && hour < 11 && min < 11) return 0;
     return day * 24 * 60 + hour * 60 + min;
 }
 
@@ -15,7 +15,7 @@ int main() {
     // Please write your code here.
     int total_min = date_to_min(a, b, c);
     if (total_min) cout << total_min - date_to_min(11, 11, 11);
-    else cout << total_min;
+    else cout << -1;
 
     return 0;
 }
